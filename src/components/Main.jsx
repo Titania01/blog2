414448760fb92ed4f518a8ebbf6e9dfbb2b37d17
Titania01 <adegbitebidmus@gmail.com>
2021-01-28 12:02:08 +0100

    update code base

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,8 @@
+import React from "react"
+
 const Main = () => {
   return (
-    <div></div>
+    <div>hello</div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,24 @@
+import React from "react"
+
 const Navbar = () => {
   return (
-    <div></div>
+    <nav className="Navbar">
+      <a href="#top">
+        <img src="https://mattdowney.co/wp-content/themes/mdv4/img/img-logo-light-bg.svg" alt=""/>
+      </a>
+      <span className="spacer" />
+      <a href="#about">
+        <span>About</span>
+      </a>
+      <a href="#blog" className="active">
+        <span>Blog</span>
+      </a>
+      <a href="#newsletter">
+        <span>Newsletter</span>
+      </a>
+    </nav>
   )
+      
 }
 
 export default Navbar
