de76fec14a39880b44f4f4aebc8dfe7784f554cc
Titania01 <adegbitebidmus@gmail.com>
2021-01-30 15:43:16 +0100

    feat: update code base

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,12 @@ const Footer = () => {
   return (
     <div className="Footer">
       <div className="one">
-        one
+        <img src="https://mattdowney.co/wp-content/themes/mdv4/img/logo-bg.svg" alt=""/>
+        <h4>Digital is in my DNA <sup>TM</sup>  <br/> © 2005 - 2021
+        </h4>
+        <span><img src="https://mattdowney.co/wp-content/themes/mdv4/img/icon-instagram-footer.svg" alt=""/>
+        <img src="https://mattdowney.co/wp-content/themes/mdv4/img/icon-twitter-footer.svg" alt="" className="last" />
+        </span>
       </div>
       <div className="two"></div>
       <div className="three"></div>
